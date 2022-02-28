ff39b45f411cf35b6b5151f538978f3c543bd75b
tanianasrin1 <tasrineva169@gmail.com>
2022-03-01 00:11:58 +0600

    card

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 const searchPhnone = () => {
     const searchInput = document.getElementById('search-phone')
     const searchInputValue = searchInput.value
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchInputValue}`
+    searchInput.value = ''
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchInputValue}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaysearchphone(data.data))
 }
+
+const displaysearchphone = (phones) => {
+    const searchResult = document.getElementById('search-result')
+    searchResult.innerHTML = ''
+    phones.forEach(phone => {
+        console.log(phone)
+        const div = document.createElement('div')
+        div.classList.add('col-lg-4')
+        div.classList.add('mt-5')
+        div.innerHTML =` 
+        <div class="card" style="width: 18rem;">
+  <img src="${phone.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${phone.phone_name}</h5>
+    <p class="card-text">${phone.brand}</p>
+    <button onclick = "cardDetails()" class="btn btn-primary">Details</button>
+  </div>
+</div>
+        `
+        
+        searchResult.appendChild(div) 
+    })
+}
+
